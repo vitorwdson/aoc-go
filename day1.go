359865b3cf242bba9746d202fd87a80ce085ce4f
Vitor Wdson <vitor.wdson2@gmail.com>
2023-04-27 21:21:21 -0400

    Part 2 of day 1 complete

--- day1.go
+++ day1.go
@@ -4,33 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
 func main() {
-    file, err := os.Open("inputs/day1.input")
-    if err != nil {
-        panic(err)
-    }
+	file, err := os.Open("inputs/day1.input")
+	if err != nil {
+		panic(err)
+	}
 
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-    totalPerElf := 0
-    max := 0
+	totalPerElf := 0
+	topElves := []int{0, 0, 0}
 
-    for scanner.Scan() {
-        line := scanner.Text()
-        calories, err := strconv.Atoi(line)
-        if err == nil {
-            totalPerElf += calories
-        } else {
-            if max < totalPerElf {
-                max = totalPerElf
-            }
-            totalPerElf = 0
-        }
-    }
+	for scanner.Scan() {
+		line := scanner.Text()
+		calories, err := strconv.Atoi(line)
+		if err == nil {
+			totalPerElf += calories
+		} else {
+			topElves = append(topElves, totalPerElf)
+			sort.Ints(topElves)
+			topElves = topElves[1:]
+			totalPerElf = 0
+		}
+	}
 
-    fmt.Printf("The elf carrying the most calories is carrying %d calories", max)
+    topElvesSum := topElves[0] + topElves[1] + topElves[2]
+	fmt.Printf("The elf carrying the most calories is carrying %d calories\n", topElves[2])
+	fmt.Printf("The top elves are carrying %d calories combined\n", topElvesSum)
 }
