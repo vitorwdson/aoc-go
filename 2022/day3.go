7ea25d97890fefd1f8dbc330eb2069bcbe9dd30c
Vitor Wdson <vitor.wdson2@gmail.com>
2023-04-29 10:19:13 -0400

    Part 2 of day 3 2022 done

--- 2022/day3.go
+++ 2022/day3.go
@@ -6,6 +6,17 @@ import (
 	"os"
 )
 
+func calculateItemPriority(item rune) int {
+	var itemPriority int
+	if item <= 90 {
+		itemPriority = int(item) - 65 + 27
+	} else {
+		itemPriority = int(item) - 97 + 1
+	}
+
+	return itemPriority
+}
+
 func Day3() {
 	file, err := os.Open("./inputs/2022/day3.input")
 	if err != nil {
@@ -16,6 +27,10 @@ func Day3() {
 	scanner.Split(bufio.ScanLines)
 
 	totalItemPriorities := 0
+    totalBadgePriorities := 0
+
+    groupItemsSet := map[rune]bool{}
+    elfIndexPerGroup := 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		compartmentSize := len(line) / 2
@@ -36,19 +51,44 @@ func Day3() {
 			}
 		}
 
-		if sharedItem == 0 {
-			continue
+		if sharedItem != 0 {
+            itemPriority := calculateItemPriority(sharedItem)
+			totalItemPriorities += itemPriority
 		}
 
-		var itemPriority int
-		if sharedItem <= 90 {
-			itemPriority = int(sharedItem) - 65 + 27
-		} else {
-			itemPriority = int(sharedItem) - 97 + 1
-		}
 
-		totalItemPriorities += itemPriority
+        // Part 2
+        elfIndexPerGroup += 1
+        
+        var groupBadge rune
+        if elfIndexPerGroup == 1 {
+            for _, char := range line {
+                groupItemsSet[char] = false
+            }
+        } else if elfIndexPerGroup == 2 {
+            for _, char := range line {
+                if _, exists := groupItemsSet[char]; exists {
+                    groupItemsSet[char] = true 
+                }
+            }
+        } else {
+            for _, char := range line {
+                if groupItemsSet[char] {
+                    groupBadge = char
+                    break
+                }
+            }
+
+            elfIndexPerGroup = 0
+            groupItemsSet = make(map[rune]bool)
+        }
+
+        if groupBadge != 0 {
+            badgePriority := calculateItemPriority(groupBadge)
+            totalBadgePriorities += badgePriority
+        }
 	}
 
 	fmt.Printf("The sum of the shared item priorities is %d\n", totalItemPriorities)
+	fmt.Printf("The sum of the badge item priorities is %d\n", totalBadgePriorities)
 }
