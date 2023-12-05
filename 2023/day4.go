a799701f7cac735359d7522eea0497e70f1759b4
Vitor Wdson <vitor.wdson2@gmail.com>
2023-12-04 21:21:00 -0400

    Part 2 of day 4 2023 done

--- 2023/day4.go
+++ 2023/day4.go
@@ -21,6 +21,9 @@ func Day4() {
 	scanner.Split(bufio.ScanLines)
 
 	totalPoints := 0
+	cardCopies := map[int]int{}
+	lastCard := 0
+
 	for scanner.Scan() {
 		line := scanner.Text() + " "
 
@@ -28,6 +31,21 @@ func Day4() {
 		pipeIndex := strings.Index(line, "|")
 
 		numberString := ""
+		for _, c := range line[:colonIndex] {
+			if !utils.IsDigit(c) {
+				continue
+			}
+			numberString += string(c)
+		}
+
+		cardNumber, err := strconv.Atoi(numberString)
+		if err != nil {
+			panic(err)
+		}
+
+		lastCard = cardNumber
+		numberString = ""
+
 		winningNumbers := map[int]bool{}
 		for _, c := range line[colonIndex+1 : pipeIndex] {
 			if utils.IsDigit(c) {
@@ -73,11 +91,26 @@ func Day4() {
 			numberString = ""
 		}
 
-
-		if matchCount > 0 {
-			totalPoints += int(math.Pow(2, float64(matchCount - 1)))
+		if matchCount != 0 {
+			totalPoints += int(math.Pow(2, float64(matchCount-1)))
 		}
+
+		cardCopies[cardNumber]++
+		for i := 1; i <= matchCount; i++ {
+			cardCopies[cardNumber+i] += cardCopies[cardNumber]
+		}
+
+	}
+
+	totalCards := 0
+	for k, v := range cardCopies {
+		if k > lastCard {
+			continue
+		}
+
+		totalCards += v
 	}
 
 	fmt.Println("Total points:", totalPoints)
+	fmt.Println("Total scratchcards:", totalCards)
 }
